e255e06e38673e921aef57d640e6a9776b8b7b5d
Simon Bäckman <simon@bckmn.se>
2025-09-03 16:13:49 +0200

    Add MDX plugin

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -111,7 +111,9 @@ return {
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
 			cssls = {},
-			ts_ls = {},
+			ts_ls = {
+				filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact", "mdx" },
+			},
 			gopls = {},
 			lua_ls = {
 				settings = {
@@ -129,7 +131,6 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
-			"typescript-language-server",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
