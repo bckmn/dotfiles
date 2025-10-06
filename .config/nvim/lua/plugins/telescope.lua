a49364e1cda697ea82c370d2251d2bea9c749cdf
Simon Bäckman <simon@bckmn.se>
2025-10-06 21:33:38 +0200

    Upgrade Telescope to remove deprecation warnings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
-	branch = "0.1.x",
+	branch = "master",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
